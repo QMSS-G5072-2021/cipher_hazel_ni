149b38047db4b8b32ce7d74ea0d6e394bfb1a800
HazelNi <91227445+HazelNi@users.noreply.github.com>
2021-11-08 19:41:54 -0500

    Update cipher_wn2188_b.py

--- src/cipher_wn2188_b/cipher_wn2188_b.py
+++ src/cipher_wn2188_b/cipher_wn2188_b.py
@@ -1,4 +1,24 @@
 def cipher(text, shift, encrypt=True):
+    """
+    Enciphers a text string with a given shift number.
+    Parameters
+    ----------
+    text : str
+    shift : int
+    encrypt : bool
+    Returns
+    -------
+    new_text
+      The new enciphered string.
+    Examples
+    --------
+    >>> from cipher_wn2188 import cipher_wn2188
+    >>> text = "Hello"
+    >>> shift = 1
+    >>> cipher_wn2188.cipher(text, shift)
+    "Ifmmp"
+    
+    """
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
     for c in text:
